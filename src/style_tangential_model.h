e3678442ef1ea0d0f44837af6ef11b121d97408c
Yidong Xia <yidong.xia@inl.gov>
2021-09-08 10:39:30 -0600

    Removed style_*.h files

--- src/style_tangential_model.h
+++
@@ -1,5 +0,0 @@
-#include "tangential_model_base.h"
-#include "tangential_model_history.h"
-#include "tangential_model_hysteretic_nonlinear.h"
-#include "tangential_model_luding_tn.h"
-#include "tangential_model_no_history.h"
